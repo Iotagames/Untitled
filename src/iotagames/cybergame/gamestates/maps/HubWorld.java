7182dc30189e038751103bfd84d186a286773db0
Sean Kelly <gamesbyiota@gmail.com>
2011-12-17 14:55:58 -0700

    Removed enemy from the hubworld.
    Signed-off-by: Sean Kelly <gamesbyiota@gmail.com>

--- src/iotagames/cybergame/gamestates/maps/HubWorld.java
+++ src/iotagames/cybergame/gamestates/maps/HubWorld.java
@@ -7,16 +7,14 @@ import iotagames.cybergame.gamestates.TileMapState;
 import iotagames.cybergame.utilities.Camera;
 
 public class HubWorld extends TileMapState {
-    Enemy enemy = new Enemy("bug2", 400, 400);
+	Enemy enemy = new Enemy("bug2", 400, 400);
     NPC npc = new NPC("npc_ud", 256, 75);
     NPC npca = new NPC("npc_ud", 224, 75);
     NPC npcb = new NPC("npc_left", 96, 256);
-    
 	public HubWorld() {
 		super("hubworld");
 	    player = new Player("player", 300, 300);
 		entities.add(player);
-		entities.add(enemy);
 		entities.add(npc);
 		entities.add(npca);
 		entities.add(npcb);
